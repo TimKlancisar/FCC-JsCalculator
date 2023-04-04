1fa1bd80a5a4492d82433f3d17858d152b4395ec
Tim Klančišar <tim.klancisar@gmail.com>
2023-04-04 22:44:51 +0200

    Complete rework

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { FaFreeCodeCamp } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
+    <div class="calculator">
+        <span class="title"><FaFreeCodeCamp /> Calculator</span>
+        <div id="display">0</div>
+        <ul class="keyboard">
+            <li id="clear"><button>C</button></li>
+            <li id='delete'><button>DEL</button></li>
+            <li id="devide"><button>/</button></li>
+            <li id="multiply"><button>*</button></li>
+            <li id="seven"><button>7</button></li>
+            <li id="eigth"><button>8</button></li>
+            <li id="nine"><button>9</button></li>
+            <li id="substract"><button>-</button></li>
+            <li id="four"><button>4</button></li>
+            <li id="five"><button>5</button></li>
+            <li id="six"><button>6</button></li>
+            <li id="add"><button>+</button></li>
+            <li id="one"><button>1</button></li>
+            <li id="two"><button>2</button></li>
+            <li id="tree"><button >3</button></li>
+            <li id="equals" class="tall-btn"><button>=</button></li>
+            <li id="zero" class="wide-btn"><button>0</button></li>
+            <li id='decimal'><button>.</button></li>
+        </ul>
     </div>
   );
 }
